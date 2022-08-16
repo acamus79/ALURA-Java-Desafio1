bdd9692f85aebc91d063500e7c025604da1a0f53
Adrian Camus <adrianedgardocamus@gmail.com>
2022-08-16 12:10:13 -0300

    Create Peso.java

---
+++ Conversor/src/conversor/conversores/Peso.java
@@ -0,0 +1,60 @@
+/*
+ * Desafio Alura Java 1
+ */
+package conversor.conversores;
+
+import java.text.DecimalFormat;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author https://github.com/acamus79
+ */
+public class Peso {
+    
+    public String valor(Double val){
+        Object[] options = {"Libra", "Onza", "Gramo"};
+        int x = JOptionPane.showOptionDialog(null, "Selecciones a que convertir",
+                "Conversion de Masa/Peso",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, new ImageIcon("assets/pesoMedida.png"), options, options[0]);
+       
+        Double resultado = 0.0;
+        String ret = "";
+        DecimalFormat df = new DecimalFormat("#,##0.000");
+        switch (x) {
+            case 0:
+                resultado = this.conversion(val,0);
+                ret = val.toString()+" Kg en Libras es = "+df.format(resultado);
+                break;
+            case 1:
+                resultado = this.conversion(val,1);
+                ret = val.toString()+" Kg en Onzas es = "+df.format(resultado);
+                break;
+            case 2:
+                resultado = this.conversion(val,2);
+                ret = val.toString()+" Kg en Gramos es = "+df.format(resultado);
+                break;
+        }
+        
+        return ret;
+        
+    }
+    
+    private Double conversion(Double val, int op){
+        Double res = 0.0;
+        switch (op){
+            case 0:
+                res = val * 2.205;
+                break;
+            case 1:
+                res = val * 35.274;
+                break;
+            case 2:
+                res = val * 1000;
+                break;
+        }
+        return res;
+    }
+    
+}
