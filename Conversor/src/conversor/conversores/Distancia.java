a0fd7e1f5f3382a957bd5666b2e51ac237b5be5e
Adrian Camus <adrianedgardocamus@gmail.com>
2022-08-16 12:10:10 -0300

    Create Distancia.java

---
+++ Conversor/src/conversor/conversores/Distancia.java
@@ -0,0 +1,59 @@
+/*
+ * Desafio Alura Java 1
+ */
+package conversor.conversores;
+
+import java.text.DecimalFormat;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author https://github.com/acamus79
+ */
+public class Distancia {
+    
+    public String valor(Double val){
+        Object[] options = {"Milla's", "Yarda's", "Pulgada's"};
+        int x = JOptionPane.showOptionDialog(null, "Selecciones a que convertir",
+                "Conversion de Distancia",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, new ImageIcon("assets/pesoMedida.png"), options, options[2]);
+       
+        Double resultado;
+        String ret = "";
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        switch (x) {
+            case 0:
+                resultado = this.conversion(val,0);
+                ret = val.toString()+" mts en Millas es = "+df.format(resultado);
+                break;
+            case 1:
+                resultado = this.conversion(val,1);
+                ret = val.toString()+" mts en Yardas es = "+df.format(resultado);
+                break;
+            case 2:
+                resultado = this.conversion(val,2);
+                ret = val.toString()+" mts en Pulgadas es = "+df.format(resultado);
+                break;
+        }
+        
+        return ret;
+        
+    }
+    
+    private Double conversion(Double val, int op){
+        Double res = 0.0;
+        switch (op){
+            case 0:
+                res = val / 1609;
+                break;
+            case 1:
+                res = val * 1.094;
+                break;
+            case 2:
+                res = val * 39.37;
+                break;
+        }
+        return res;
+    }
+}
