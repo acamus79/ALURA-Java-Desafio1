ae269bca7f0b9bdb01d71ff9437e608c667b4e28
Adrian Camus <adrianedgardocamus@gmail.com>
2022-08-16 12:09:42 -0300

    First Commit

---
+++ Conversor/src/conversor/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Desafio Alura Java 1
+ */
+package conversor;
+
+import conversor.menu.Menu;
+
+
+/**
+ *
+ * @author https://github.com/acamus79
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Menu m = new Menu();
+        m.menu();
+        
+    }
+    
+}
